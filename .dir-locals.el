22a11c16d30196bc91cf61b6efcf8b6da144cf47
rlouf <remilouf@protonmail.com>
2022-04-06 08:11:36 +0000

    Deploying to gh-pages from  @ 0b4c6c0e447a3fdd302c46fcfc771ccbb9642702 🚀

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . ((projectile-project-compilation-cmd . "make publish")
-         (org-roam-directory . "/home/remi/projects/thetypicalset/posts")
-         (org-roam-db-location . "/home/remi/projects/thetypicalset/posts/org-roam.db"))))
