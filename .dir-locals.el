a75075739e9d8b32dd9462792b76105d5815498c
Rémi Louf <remilouf@gmail.com>
2022-05-27 10:35:18 +0200

    Bonjour

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((nil . ((projectile-project-compilation-cmd . "make publish")
+         (org-download-image-dir . "~/projects/thetypicalset/org/img")
          (org-roam-directory . "/home/remi/projects/thetypicalset/org")
          (org-roam-db-location . "/home/remi/projects/thetypicalset/org/org-roam.db"))))
