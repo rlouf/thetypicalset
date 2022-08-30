f2d1ff7f609dea05e67412ba1bc41d3c004890d9
Rémi Louf <remilouf@gmail.com>
2022-08-30 15:59:02 -0600

    Remove tag on garden design

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((nil . ((eval . (set (make-local-variable 'project-path) (locate-dominating-file default-directory ".dir-locals.el")))
          (eval . (setq org-roam-directory (concat project-path "org/")))
-         (eval . (setq org-download-image-dir (concat project-path "img")))
-         (eval . (setq org-roam-db-location (concat project-path "org-roam.db"))))))
+         (eval . (setq org-download-image-dir (concat project-path "org/img")))
+         (eval . (setq org-roam-db-location (concat project-path "org/org-roam.db"))))))
