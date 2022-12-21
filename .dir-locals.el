59380005df5b01fb064d22821822b5805283bd56
Rémi Louf <remi@thetypicalset.com>
2022-12-21 09:52:29 +0100

    Change project path variable name

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
-((nil . ((eval . (set (make-local-variable 'project-path) (locate-dominating-file default-directory ".dir-locals.el")))
-         (eval . (setq org-roam-directory (concat project-path "org/")))
-         (eval . (setq org-download-image-dir (concat project-path "org/img")))
-         (eval . (setq org-roam-db-location (concat project-path "org/org-roam.db"))))))
+((nil . ((eval . (set (make-local-variable 'org-project-path) (locate-dominating-file default-directory ".dir-locals.el")))
+         (eval . (setq org-roam-directory (concat org-project-path "org/")))
+         (eval . (setq org-download-image-dir (concat org-project-path "org/img")))
+         (eval . (setq org-roam-db-location (concat org-project-path "org/org-roam.db"))))))
